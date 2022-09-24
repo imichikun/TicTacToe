c183ab1a3000a498df6e25be1f2e503774272c54
Rustem <39364256+imichikun@users.noreply.github.com>
2022-09-24 22:11:24 +0500

    v1.2

--- BaseClass.java
+++ BaseClass.java
@@ -4,21 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Stream;
 
 public class BaseClass {
     private static int[][] arrayMock = new int[][] { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    private static String[][] array = new String[3][3];
-    private static String PLAYER1;
+    private static String[][] array = new String[][] { {".", ".", "."}, {".", ".", "."}, {".", ".", "."}};
+    private static String PLAYER1 = "Rust";
     private static String PLAYER2 = "Computer";
+    static boolean gameOver = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.print("Пожайлуста, введите своё имя: ");
 //        enterName();
         System.out.println("Первому игроку " + PLAYER1 + " приготовиться!");
         System.out.println("Вы играете крестиками, сделайте свой ход на клетку от 1 до 9");
         Arrays.stream(arrayMock).forEach(elem -> System.out.println(Arrays.toString(elem)));
         System.out.print("Введите свой ход: ");
-        makeMove();
+        playerMove();
     }
 
     private static void enterName() {
@@ -34,57 +38,116 @@ public class BaseClass {
         PLAYER1 = name;
     }
 
-    private static void makeMove() {
+    private static void playerMove() throws IOException {
         int number = 0;
 
         try (BufferedReader reader2 = new BufferedReader(new InputStreamReader(System.in)))
         {
             String move = reader2.readLine();
-            number = checkTheNumberOfMove(Integer.parseInt(move));
+            number = checkTheEnteredNumber(Integer.parseInt(move));
         } catch (IOException e){
             System.out.println("Ошибка ввода хода");
             e.printStackTrace();
         }
 
-//        checkTheMove(number);
-
         switch(number){
-            case 1 -> checkTheMove(array[0][0]);
-//            {
-//                if (array[0][0] == null)
-//                                array[0][0] = "X";
-//            }
-//            case 2 -> array[0][1] = "X";
-//            case 3 -> array[0][2] = "X";
-//            case 4 -> array[1][0] = "X";
-//            case 5 -> array[1][1] = "X";
-//            case 6 -> array[1][2] = "X";
-//            case 7 -> array[2][0] = "X";
-//            case 8 -> array[2][1] = "X";
-//            case 9 -> array[2][2] = "X";
+            case 1 -> checkIfCellIsEmpty(array[0], 0);
+            case 2 -> checkIfCellIsEmpty(array[0], 1);
+            case 3 -> checkIfCellIsEmpty(array[0], 2);
+            case 4 -> checkIfCellIsEmpty(array[1], 0);
+            case 5 -> checkIfCellIsEmpty(array[1], 1);
+            case 6 -> checkIfCellIsEmpty(array[1], 2);
+            case 7 -> checkIfCellIsEmpty(array[2], 0);
+            case 8 -> checkIfCellIsEmpty(array[2], 1);
+            case 9 -> checkIfCellIsEmpty(array[2], 2);
         }
     }
 
-    private static String checkTheMove(String value) {
-        if (value != null) {
+    private static void checkIfCellIsEmpty(String[] massiv, int number) throws IOException {
+        checkWinCondition();
+
+        if ( !massiv[number].equals(".") ) {
             System.out.println("Поле уже занято ! Повторите ход...");
             showGameResults();
-            makeMove();
-        }
-        value = "X";
-        return value;
+            playerMove();
+        } else
+            massiv[number] = "X";
+
+        showGameResults();
+        computerMove();
     }
 
-    private static int checkTheNumberOfMove(int number){
+    private static void computerMove() throws IOException {
+//        checkWinCondition();
+
+        System.out.println("Компьютер делает свой ход...");
+        if (array[1][1].equals(".")) {
+            array[1][1] = "O";
+        }
+
+        else if (array[0][0].equals(".") || array[0][2].equals(".") || array[2][0].equals(".") || array[2][2].equals(".")){
+            int random = new Random().nextInt(3);
+            switch(random){
+                case 0 -> array[0][0] = "O";
+                case 1 -> array[0][2] = "O";
+                case 2 -> array[2][0] = "O";
+                case 3 -> array[2][2] = "O";
+            }
+        }
+
+        else if (array[0][1].equals(".") || array[1][0].equals(".") || array[1][2].equals(".") || array[2][1].equals(".")){
+            int random = new Random().nextInt(3);
+            switch(random){
+                case 0 -> array[0][1] = "O";
+                case 1 -> array[1][0] = "O";
+                case 2 -> array[1][2] = "O";
+                case 3 -> array[2][1] = "O";
+            }
+        }
+
+        playerMove();
+    }
+
+    private static int checkTheEnteredNumber(int number) throws IOException {
         if (number < 1 || number > 9) {
             System.out.println("Ход должен быть в диапазоне от 1 до 9");
             System.out.print("Введите свой ход: ");
-            makeMove();
+            playerMove();
         }
         return number;
     }
 
+    private static void checkWinCondition() throws IOException {
+        if (array[0][0].equals("X") & array[0][1].equals("X") & array[0][2].equals("X") )
+                gameOver = true;
+        else if (array[1][0].equals("X") & array[1][1].equals("X") & array[1][2].equals("X") )
+            gameOver = true;
+        else if (array[2][0].equals("X") & array[2][1].equals("X") & array[2][2].equals("X") )
+            gameOver = true;
+        else if (array[0][0].equals("X") & array[1][0].equals("X") & array[2][0].equals("X") )
+            gameOver = true;
+        else if (array[0][1].equals("X") & array[1][1].equals("X") & array[2][1].equals("X") )
+            gameOver = true;
+        else if (array[0][2].equals("X") & array[1][2].equals("X") & array[2][2].equals("X") )
+            gameOver = true;
+        else if (array[0][0].equals("X") & array[1][1].equals("X") & array[2][2].equals("X") )
+            gameOver = true;
+        else if (array[2][0].equals("X") & array[1][1].equals("X") & array[0][2].equals("X") )
+            gameOver = true;
+
+//        if ( Arrays.stream(array).flatMap(Stream::of).noneMatch(elem -> elem.equals(".")) )
+//            gameOver = true;
+        endOfGame();
+    }
+
     private static void showGameResults(){
         Arrays.stream(array).forEach(elem -> System.out.println(Arrays.toString(elem)));
+    }
+
+    private static void endOfGame() throws IOException {
+        if (gameOver == true){
+            System.out.println("Игра закончена, игрок " + PLAYER1 + " победил");
+        }
+        computerMove();
     }
 }

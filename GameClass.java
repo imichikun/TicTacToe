e38a5e8b3e992d6b294b267fb24d7b80fabfd9a0
Rustem <39364256+imichikun@users.noreply.github.com>
2022-09-27 18:53:24 +0500

    nonstatic v5 commit

--- GameClass.java
+++ GameClass.java
@@ -11,21 +11,22 @@ import static TicTacToe.UtilClass.showGameResults;
 public class GameClass {
     static BufferedReader reader;
     static int countOfOperations;
+    static int[] arrayOfComputerMoves = new int[9];
 
-    void playerMove() throws IOException {
+    void playerMove() {
         System.out.print("Введите свой ход: ");
         int number = enterNextMove();
 
-        switch(number){
-            case 1 -> checkIfCellIsEmpty(array[0], 0);
-            case 2 -> checkIfCellIsEmpty(array[0], 1);
-            case 3 -> checkIfCellIsEmpty(array[0], 2);
-            case 4 -> checkIfCellIsEmpty(array[1], 0);
-            case 5 -> checkIfCellIsEmpty(array[1], 1);
-            case 6 -> checkIfCellIsEmpty(array[1], 2);
-            case 7 -> checkIfCellIsEmpty(array[2], 0);
-            case 8 -> checkIfCellIsEmpty(array[2], 1);
-            case 9 -> checkIfCellIsEmpty(array[2], 2);
+        switch (number){
+            case 1 ->   checkIfCellIsEmpty(array[0], 0);
+            case 2 ->   checkIfCellIsEmpty(array[0], 1);
+            case 3 ->   checkIfCellIsEmpty(array[0], 2);
+            case 4 ->   checkIfCellIsEmpty(array[1], 0);
+            case 5 ->   checkIfCellIsEmpty(array[1], 1);
+            case 6 ->   checkIfCellIsEmpty(array[1], 2);
+            case 7 ->   checkIfCellIsEmpty(array[2], 0);
+            case 8 ->   checkIfCellIsEmpty(array[2], 1);
+            case 9 ->   checkIfCellIsEmpty(array[2], 2);
         }
     }
 
@@ -42,8 +43,8 @@ public class GameClass {
         return number;
     }
 
-    void checkIfCellIsEmpty(String[] massiv, int number) throws IOException {
-        checkWinCondition();
+    void checkIfCellIsEmpty(String[] massiv, int number) {
+//        checkWinCondition();
 
         if ( !massiv[number].equals(".") ) {
             System.out.println("Поле уже занято ! Повторите ход...");
@@ -53,21 +54,25 @@ public class GameClass {
             massiv[number] = "X";
 
         showGameResults();
-        checkWinCondition();
-//        computerMove();
+//        checkWinCondition();
+        computerMove();
     }
 
-    void computerMove() throws IOException {
+    void computerMove() {
         checkWinCondition();
         System.out.println("Компьютер делает свой ход...");
 
         if (array[1][1].equals(".")) {
             array[1][1] = "O";
+            showGameResults();
+            playerMove();
         }
 
-        else if (array[0][0].equals(".") || array[0][2].equals(".") || array[2][0].equals(".") || array[2][2].equals(".")){
-            System.out.println("1 else if");
-            int random = new Random().nextInt(3);
+        int random = new Random().nextInt(3);
+
+        for (int elem: arrayOfComputerMoves)
+            if (random != elem && array[0][0].equals(".") || array[0][2].equals(".") || array[2][0].equals(".")
+                    || array[2][2].equals(".")) {
 
             switch(random){
                 case 0 : if ( !array[0][0].equals(".") )
@@ -136,7 +141,7 @@ public class GameClass {
                 }
             }
         }
-        checkWinCondition();
+//        checkWinCondition();
     }
 
     void checkWinCondition() {
@@ -204,21 +209,21 @@ public class GameClass {
             gameOver = true;
         }
 
-//        if ( Arrays.stream(array).flatMap(Stream::of).noneMatch(elem -> elem.equals(".")) )
-//            gameOver = true;
+        else if ( !array[0][0].equals(".") && !array[0][1].equals(".") && !array[0][2].equals(".") &&
+                !array[1][0].equals(".") && !array[1][1].equals(".") && !array[1][2].equals(".") &&
+                !array[2][0].equals(".") && !array[2][1].equals(".") && !array[2][2].equals(".") ) {
+                    System.out.println("Игра закончена - ничья !");
+                    System.exit(0);
+            }
+
         endOfGame();
     }
 
     int checkTheEnteredNumber(int number) {
         if (number < 1 || number > 9) {
             System.out.println("Ход должен быть в диапазоне от 1 до 9");
-            try{
                 playerMove();
-            } catch (IOException e){
-                System.out.println("Ошибка в checkEnteredNumber");
-                e.printStackTrace();
             }
-        }
         return number;
     }
 
